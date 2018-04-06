4def984b5161e5d1ee0ba0d020c1469285e9a1ea
Playingtygre <kymiehsu123@gmail.com>
2018-04-05 20:57:13 -0700

    adding controller, and model view

---
+++ Person_of_the_year/Person_of_the_year/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Person_of_the_year.Controllers
+{
+    public class HomeController : Controller
+    { //methods inside controllers are called actions
+
+
+      
+        
+        public ViewResult Index (int id)
+        {
+            string name = "Tiger";
+            return View(name);
+        }
+        
+    }
+}
