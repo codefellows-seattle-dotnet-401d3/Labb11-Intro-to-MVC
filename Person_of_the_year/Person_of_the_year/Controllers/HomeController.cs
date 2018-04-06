43ee27021365d8371647d2cefb12f5b1ffe16d07
Playingtygre <kymiehsu123@gmail.com>
2018-04-05 22:14:26 -0700

    adding file string

--- Person_of_the_year/Person_of_the_year/Controllers/HomeController.cs
+++ Person_of_the_year/Person_of_the_year/Controllers/HomeController.cs
@@ -6,17 +6,31 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Person_of_the_year.Controllers
 {
+
+    /* Portions of this code was copied from 
+     Amanda Iverson and Phil Werner
+         */
+
     public class HomeController : Controller
     { //methods inside controllers are called actions
 
-
-      
+        [HttpGet]
         
         public ViewResult Index (int id)
         {
             List<string> myList = new List<string>() { };
             return View();
         }
+
+
+        /* This is a posting method takes in two arguments integers
+        and redirects to the Results page */
+        [HttpPost]
+        public IActionResult Index(int StartYear, int EndYear)
+        {
+            return RedirectToAction("Results", new { StartYear, EndYear });
+        }
+
         
     }
 }
